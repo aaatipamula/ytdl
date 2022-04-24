e07890bb5c4d0d28e0b021f13fd020794e2ac9a1
Aniketh A <aaatipamula@gmail.com>
2022-04-23 23:47:41 -0500

    moved around files, made changes to bot code in order to recognize youtube urls properly

--- bot-framework/downloader.py
+++ bot-framework/downloader.py
@@ -1,5 +1,7 @@
+from email.mime import base
 import yt_dlp
 import os
+import base64
 
 class Downloader():
     def __init__(self, url, music_dir, video_dir):
@@ -11,6 +13,8 @@ class Downloader():
             info = ydl.extract_info(url, download=False)
         
         self.video_title = info.get('title', None)
+        self.video_title_base64 = base64.urlsafe_b64encode(self.video_title.encode("utf-8"))
+        self.base64_string = self.video_title_base64.decode('utf-8')
 
     def download_audio(self):
         os.chdir(self.music_dir)
@@ -18,7 +22,7 @@ class Downloader():
 
         ydl_opts = {
             'format': 'bestaudio/best',
-            'outtmpl': '%(title)s.%(ext)s',
+            'outtmpl': f'{self.base64_string}.%(ext)s',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -38,7 +42,8 @@ class Downloader():
             'outtmpl':'%(title)s.%(ext)s',
             'postprocessors': [{
                 'key':'FFmpegVideoConvertor',
-                'preferedformat':'mp4',
+                'preferredformat':'mp4',
+                'perferredquality':'1080p'
             }],
         }
 
