1355c7af196db10d3b3f396711791ded17a73696
Aniketh A <aaatipamula@gmail.com>
2022-06-22 16:17:02 -0500

    changes lol

--- file-server/routes/audio.js
+++ file-server/routes/audio.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
+
+router.get("/", (req, res) =>{
+    res.send(403)
+})
 
 router.route("/:fileid").get(
     (req, res) => {
-        res.send(`/downloads/audio/${req.params.fileid}`)
+
+        let fileid = req.params.fileid
+
+        fs.access(`downloads/audio/${fileid}.mp3`, fs.constants.F_OK, (err) => {
+
+            if (err) {
+                res.sendStatus(404)
+
+            }else{
+                res.download(`downloads/audio/${fileid}.mp3`, `${Buffer.from(fileid, 'base64').toString()}.mp3`)
+
+            }
+        });
     }
 )
 
